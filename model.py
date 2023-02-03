70ff69af5563a59e78f77cd43bec04bb95c83a53
b06b01073 <b06b01073@gmail.com>
2023-02-03 20:57:26 +0800

    loss net: vgg16

--- model.py
+++ model.py
@@ -1,4 +1,8 @@
 from torch import nn
+from torchvision.models import vgg16
+from torchvision.models.feature_extraction import create_feature_extractor 
+from torchvision.utils import save_image
+import utils
 
 class TransformNet(nn.Module):
     def __init__(self):
@@ -11,44 +15,39 @@ class TransformNet(nn.Module):
             nn.BatchNorm2d(32),
             nn.ReLU(),
 
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=2, padding=2),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
 
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=2),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(),
 
             self.res_blocks[0],
-            nn.BatchNorm2d(128),
             nn.ReLU(),
 
             self.res_blocks[1],
-            nn.BatchNorm2d(128),
             nn.ReLU(),
 
             self.res_blocks[2],
-            nn.BatchNorm2d(128),
             nn.ReLU(),
 
             self.res_blocks[3],
-            nn.BatchNorm2d(128),
             nn.ReLU(),
 
             self.res_blocks[4],
-            nn.BatchNorm2d(128),
             nn.ReLU(),
             
-            nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=3, stride=2, padding=1),
+            nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=3, stride=2, padding=2),
             nn.BatchNorm2d(64),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=3, stride=2, padding=1),
+            nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=3, stride=2, padding=2),
             nn.BatchNorm2d(32),
             nn.ReLU(),
 
             nn.Conv2d(in_channels=32, out_channels=3, kernel_size=9, stride=1, padding=4),
-            nn.BatchNorm2d(),
+            nn.BatchNorm2d(3),
             nn.Tanh(),
         )
 
@@ -59,10 +58,22 @@ class TransformNet(nn.Module):
 
 class LossNet(nn.Module):
     def __init__(self):
-        super().__init()
+        super().__init__()
+        self.net = vgg16(weights='IMAGENET1K_V1').features
+        self.style_extractor = create_feature_extractor(self.net, {
+            '3': 'relu1_2',
+            '8': 'relu2_2',
+            '15': 'relu3_3',
+            '22': 'relu4_3',
+        })
 
-    def forward(self):
-        pass
+        self.content_extractor = create_feature_extractor(self.net, {
+            '15': 'relu3_3',
+        })
+
+
+    def forward(self, x):
+        return self.style_extractor(x), self.content_extractor(x)
 
 
 
@@ -73,13 +84,13 @@ class ResBlock(nn.Module):
         self.padding = kernel_size // 2
 
         self.net = nn.Sequential(
-            nn.Conv2d(kernel_size=kernel_size, in_channels=in_channels, out_channels=out_channels, padding=self.padding, stride=2),
+            nn.Conv2d(kernel_size=kernel_size, in_channels=in_channels, out_channels=out_channels, padding=self.padding, stride=1),
             nn.BatchNorm2d(num_features=out_channels),
             nn.ReLU(),
-            nn.Conv2d(kernel_size=kernel_size, in_channels=out_channels, out_channels=out_channels, padding=self.padding, stride=2),
+            nn.Conv2d(kernel_size=kernel_size, in_channels=out_channels, out_channels=out_channels, padding=self.padding, stride=1),
             nn.BatchNorm2d(num_features=out_channels)
         )
 
     def forward(self, x):
-        output = self.cnn(x)
+        output = self.net(x)
         return output + x
--- train.py
+++ train.py
@@ -2,13 +2,22 @@ import utils
 from torchvision.utils import save_image
 from dataset import COCODataset
 from torch.utils.data import DataLoader
+import torch
+
+from model import TransformNet
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu' 
 
 def train():
     dataset = COCODataset('./train2014')
     data_loader = DataLoader(dataset, batch_size=4, shuffle=True)
+    epochs = 10
+    transform_net = TransformNet().to(device)
 
-    imgs = next(iter(data_loader))
-    save_image(imgs[0], 'test.jpg')
+    for _ in range(epochs):
+        imgs = next(iter(data_loader)).to(device)
+        output = transform_net(imgs)
+        print(output.shape)
 
 
 if __name__ == '__main__':
